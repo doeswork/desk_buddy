7ac3ef00406b26af77f839a2ce137bbaa7a1477e
jeffdoeswork <jeff@does.work>
2023-08-02 13:42:13 -0400

    Final Code update

--- code/arduino_desk_buddy.ino
+++ code/arduino_desk_buddy.ino
@@ -6,13 +6,17 @@
 RF24 radio(9, 10); // CE, CSN
 Servo servo1;
 Servo servo2;
+Servo servo3;
+Servo servo4;
 
-int joystick[2]; // to store X and Y values
-float servoPos[2] = {90, 90}; // Start at the center, now as a float
+int joystick[4]; // to store X and Y values for two joysticks
+float servoPos[4] = {90, 90, 90, 90}; // Start at the center for all four servos
 
 void setup() {
   servo1.attach(3);
   servo2.attach(5);
+  servo3.attach(6);
+  servo4.attach(7);
   radio.begin();
   radio.openReadingPipe(1, 0xF0F0F0F0E1LL); // same unique identifier as before
   radio.setPALevel(RF24_PA_HIGH);
@@ -24,23 +28,21 @@ void loop() {
     radio.read(&joystick, sizeof(joystick)); // read the incoming data
 
     // Adjust servo position based on joystick movement
-    if(joystick[0] > 542) { // 30 units dead zone
-      servoPos[0] += (joystick[0] - 542) / 1000.0; // Change the divisor to adjust sensitivity
-    } else if(joystick[0] < 482) { // 30 units dead zone
-      servoPos[0] -= (482 - joystick[0]) / 1000.0; // Change the divisor to adjust sensitivity
-    }
-    if(joystick[1] > 542) { // 30 units dead zone
-      servoPos[1] += (joystick[1] - 542) / 1000.0; // Change the divisor to adjust sensitivity
-    } else if(joystick[1] < 482) { // 30 units dead zone
-      servoPos[1] -= (482 - joystick[1]) / 1000.0; // Change the divisor to adjust sensitivity
-    }
+    for(int i = 0; i < 4; i++) {
+      if(joystick[i] > 542) { // 30 units dead zone
+        servoPos[i] += (joystick[i] - 542) / 10000.0; // Change the divisor to adjust sensitivity
+      } else if(joystick[i] < 482) { // 30 units dead zone
+        servoPos[i] -= (482 - joystick[i]) / 10000.0; // Change the divisor to adjust sensitivity
+      }
 
-    // Constrain the servo position to 0 - 180 range
-    servoPos[0] = constrain(servoPos[0], 0, 180);
-    servoPos[1] = constrain(servoPos[1], 0, 180);
+      // Constrain the servo position to 0 - 180 range
+      servoPos[i] = constrain(servoPos[i], 0, 180);
+    }
 
     // Write the new position to the servos
     servo1.write((int)servoPos[0]); // Cast back to int for the servo function
     servo2.write((int)servoPos[1]); // Cast back to int for the servo function
+    servo3.write((int)servoPos[2]); // Cast back to int for the servo function
+    servo4.write((int)servoPos[3]); // Cast back to int for the servo function
   }
 }
--- code/arduino_remote.ino
+++ code/arduino_remote.ino
@@ -3,16 +3,26 @@
 #include <RF24.h>
 
 RF24 radio(9, 10); // CE, CSN
-int joystick[2]; // to store X and Y values
+
+int joystick1_X, joystick1_Y, joystick2_X, joystick2_Y;
+int data[4]; // To store X and Y values for two joysticks
 
 void setup() {
   radio.begin();
-  radio.openWritingPipe(0xF0F0F0F0E1LL); // unique identifier for the pipe
+  radio.openWritingPipe(0xF0F0F0F0E1LL); // same unique identifier as before
   radio.setPALevel(RF24_PA_HIGH);
 }
 
 void loop() {
-  joystick[0] = analogRead(A0); // reads the value of the X-axis
-  joystick[1] = analogRead(A1); // reads the value of the Y-axis
-  radio.write(&joystick, sizeof(joystick)); // send the joystick movement
+  joystick1_X = analogRead(A0); // read joystick1 X-axis
+  joystick1_Y = analogRead(A1); // read joystick1 Y-axis
+  joystick2_X = analogRead(A2); // read joystick2 X-axis
+  joystick2_Y = analogRead(A3); // read joystick2 Y-axis
+
+  data[0] = joystick1_X; // save X value of joystick1
+  data[1] = joystick1_Y; // save Y value of joystick1
+  data[2] = joystick2_X; // save X value of joystick2
+  data[3] = joystick2_Y; // save Y value of joystick2
+
+  radio.write(&data, sizeof(data)); // send the data
 }
