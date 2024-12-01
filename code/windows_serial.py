35ba930ec5325e831c0f0255ce5585d6e1d84fea
markdoeswork <mark@does.work>
2024-11-30 22:10:00 -0500

    Add Freecad file

--- code/windows_serial.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-# Replace 'COM_PORT' with the COM port the HC-06 is connected to
-ser = serial.Serial('COM3', 9600) 
-
-while True:
-    data = ser.readline()
-    print(data.decode('utf-8'))
